8ded3746427206dd77ed48953ae74580782a84be
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-01-13 11:19:07 -0800

    variables

---
+++ declaring_variables.cs
@@ -0,0 +1,12 @@
+//To create a new variable, 
+//you must first declare the data type of the variable, 
+//then give it a name.
+string firstName;
+//Here's a few examples of variable declarations using the data types we learned about previously.
+char userOption;
+
+int gameScore;
+
+decimal particlesPerMillion;
+
+bool processedCustomer;

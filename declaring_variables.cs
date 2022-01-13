2721f512829b742d8dd9ab72144dfe0e0d2a8c12
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-01-13 11:21:02 -0800

    Create declaring_variables.cs

---
+++ declaring_variables.cs
@@ -0,0 +1,18 @@
+//To create a new variable, 
+//you must first declare the data type of the variable, 
+//then give it a name.
+string firstName;
+//Here's a few examples of variable declarations using the data types we learned about previously.
+char userOption;
+
+int gameScore;
+
+decimal particlesPerMillion;
+
+bool processedCustomer;
+
+//Recap
+
+//Variables are temporary values you store in the computer's memory.
+//Before you can use a variable, you have to declare it.
+//To declare a variable, you first select a data type for the kind of data you want to store, and then give the variable a name that follows the rules.

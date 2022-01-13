e7d9c198943780ae37dcdaa26ff97a4ce121ef76
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-01-13 11:15:49 -0800

    data_types_c#

---
+++ datatypes.cs
@@ -0,0 +1,27 @@
+Console.WriteLine('b');
+// String
+
+Console.WriteLine("Hello World!");
+// If you want to print a numeric whole number (no fractions) value to Output, 
+//you can use an int literal. 
+//An int literal requires no additional operators like the string or char.
+
+Console.WriteLine(123);
+
+//if we wanted to print a number that includes values after the decimal point, 
+//we could use a decimal literal.
+
+//To create a decimal literal, append the letter m after the number. 
+//In this context, the m is called a literal suffix. 
+//The literal suffix tells the compiler you wish to work with a value of type decimal.
+Console.WriteLine(12.3m);
+
+//If we wanted to print a value representing either true or false, we could use a bool literal.
+
+Console.WriteLine(true);
+Console.WriteLine(false);
+//string for words, phrases, or any alphanumeric data for presentation, not calculation
+//char for a single alphanumeric character
+//int for a whole number
+//decimal for a number with a decimal
+//bool for a true/false value

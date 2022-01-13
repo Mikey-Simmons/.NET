fcbe6144c4c9077a8e4c56300e2e73cbb46eb3d1
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-01-13 01:53:32 -0800

    c

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -2,3 +2,5 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("The Current Time is" + DateTime.Now);
 Console.Write("yo");
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");

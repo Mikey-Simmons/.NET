c3bfcdb06ce61632da632b82bab84bb470a2fe26
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-01-13 11:25:53 -0800

    Create setting_and_getting_values_from_variables.cs

---
+++ Learn/setting_and_getting_values_from_variables.cs
@@ -0,0 +1,33 @@
+//Declare a variable and assign a value to it
+//To assign a value to a variable, you use the assignment operator, 
+//which is a single equals character =.
+
+string firstName;
+firstName = "Bob";
+
+//To retrieve a value from a variable, you just use the name of the variable. 
+//This example will set a variable's value, then retrieve that value and print it to the console.
+
+string lastName;
+lastName = "Berns";
+Console.WriteLine(lastName);
+
+//You can reuse and reassign the variable as many times as you want.
+//This example illustrates that idea.
+
+string firstName2;
+firstName2 = "Bob";
+Console.WriteLine(firstName2);
+firstName2 = "Beth";
+Console.WriteLine(firstName2);
+firstName2 = "Conrad";
+Console.WriteLine(firstName2);
+firstName2 = "Grant";
+Console.WriteLine(firstName2);
+
+//Recap
+//You must assign (set) a value to a variable before you can retrieve (get) a value from a variable.
+//You can initialize a variable by assigning a value to the variable at the point of declaration.
+//Assignment happens from right to left.
+//You use a single equals character as the assignment operator.
+//To retrieve the value from the variable, you merely use the variable's name.

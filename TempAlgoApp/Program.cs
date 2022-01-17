19d4d7ad4a2677dcbd894de4b80f9748a7ca8ad5
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-01-17 15:32:45 -0800

    starting_c_sharp_algos

---
+++ TempAlgoApp/Program.cs
@@ -0,0 +1,8 @@
+﻿int fahrenheit = 94;
+decimal num1 = 5;
+decimal num2 = 9;
+decimal num = (num1/num2);
+decimal celcius = (fahrenheit-32) * (num);
+
+
+Console.Write($"The temperature is "+ celcius+ " Celcius");

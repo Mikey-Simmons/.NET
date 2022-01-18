0ce4fe3ad0b1909bbb3a9d81d3ca2d44218ed24c
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-01-18 00:10:31 -0800

    Update Program.cs

--- TempAlgoApp/Program.cs
+++ TempAlgoApp/Program.cs
@@ -5,4 +5,4 @@ decimal num = (num1/num2);
 decimal celcius = (fahrenheit-32) * (num);
 
 
-Console.Write($"The temperature is "+ celcius+ " Celcius");
+Console.Write($"The temperature is "+ celcius+ " Celcius!");

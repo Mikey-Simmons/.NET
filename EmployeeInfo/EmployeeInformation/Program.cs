b31a75f438ac6c2bd0a6839d55b24d3bb8d12d10
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-01-20 12:37:19 -0800

    cshrarpWork

---
+++ EmployeeInfo/EmployeeInformation/Program.cs
@@ -0,0 +1,11 @@
+﻿string name;
+string bday;
+string phone;
+string email;
+
+name = "Lucinda Potter";
+bday = "06/24/1992";
+phone = "312-982-1010";
+email = "ewoz@woz-u.com";
+
+Console.Write($"{name}, DOB: {bday}, phone number: {phone}, email: {email}");

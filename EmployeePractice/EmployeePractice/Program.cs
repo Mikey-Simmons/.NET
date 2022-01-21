9c4a49ae3bd4454ac6c71cf89baf2aa9ef4c9315
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-01-21 06:29:58 -0800

    c#

---
+++ EmployeePractice/EmployeePractice/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("First Integer:");
+string x = Console.ReadLine();
+Console.WriteLine("Second Integer:");
+string y = Console.ReadLine();
+int i = Convert.ToInt32(x);
+int j = Convert.ToInt32(y);
+int z = i + j;
+Console.WriteLine("The sum of " +i + " + " +j +" = " + z +"!");
--- OperatorsActivity/Program.cs
+++ OperatorsActivity/Program.cs
@@ -19,3 +19,4 @@
     return isAllTrue;
 }
 
+Console.ReadLine();

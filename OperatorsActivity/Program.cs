2d8a4ed8843b657f3d5acdc81701a36b862ba23b
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-01-20 17:42:13 -0800

    csharp

---
+++ OperatorsActivity/Program.cs
@@ -0,0 +1,21 @@
+﻿
+        bool CheckEverything()
+{
+    bool bool1 = true;
+    bool bool2 = true;
+    bool bool3 = true;
+
+    bool isAllTrue;
+    if (
+        bool1 == true && bool2 == true && bool3 == true
+        )
+    {
+        isAllTrue = true;
+    }
+    else
+    {
+        isAllTrue = false;
+    }
+    return isAllTrue;
+}
+

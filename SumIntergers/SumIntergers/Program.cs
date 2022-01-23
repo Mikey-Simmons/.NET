92558598e1a8e8b550d99f1416867017661adbd0
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-01-23 00:57:45 -0800

    c#

---
+++ SumIntergers/SumIntergers/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter as many integers as you wish seperated by spaces, press return when you are ready for the sum");
+string nums = Console.ReadLine();
+for (int i = 0; i < nums.Length; i++)
+   
+{
+    Console.WriteLine(nums[i]);
+}

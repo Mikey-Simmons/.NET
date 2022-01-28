03b4be35464d7c3541e9e91f39aa32cd72b9c626
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-01-28 00:24:45 -0800

    c#

--- NBAapp/NBAapp/Program.cs
+++ NBAapp/NBAapp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+
 namespace nba
 {
     public class Program
@@ -11,7 +12,7 @@ namespace nba
             var my_player = new Player("Trae Young", "Atlanta Hawks");
             var my_player2 = new Player("Detlef Schrempf", "Seattle SuperSonics");
             var my_player3 = new Player("LeBron James", "Los Angeles Lakers",32000,1003);
-            var my_team3 = new Team("Lakers", "Los Angeles",17);
+            var my_team3 = new Team("Lakers", "Los Angeles",17,1000,600);
             List<Player> players = new List<Player>();
             players.Add(my_player);
             players.Add(my_player2);
@@ -28,6 +29,7 @@ namespace nba
             foreach (var team in teams)
             {
                 Console.WriteLine(team.getInfo());
+                Console.WriteLine(team.WinPercentage());
             }
         }
 
@@ -37,16 +39,39 @@ namespace nba
             public string Name { get; set; }
             public string Location { get; set; }
             public int RingsWon { get; set; }
+            public int GamesPlayed  { get; set; }
+            public int GamesWon { get; set; }
             public Team(string Name, string Location, int Rings)
             {
                 this.Name = Name;
                 this.Location = Location;
                 this.RingsWon = Rings;
+                this.GamesPlayed= 1;
+                this.GamesWon = 1;
+            }
+            public Team(string Name, string Location, int Rings, int GamesPlayed, int GamesWon)
+            {
+                this.Name = Name;
+                this.Location = Location;
+                this.RingsWon = Rings;
+                this.GamesPlayed = GamesPlayed;
+                this.GamesWon = GamesWon;
             }
             public string getInfo()
             {
                 return $"The {Location} {Name} have won {RingsWon} Championship(s)!";
             }
+            public string WinPercentage()
+            {
+                decimal winrate;
+                winrate= (GamesPlayed / GamesWon);
+                decimal myRoundedWR = Math.Round(winrate);
+                string my_wr = winrate.ToString();
+                return $"The {Location} {Name} have won {my_wr} of their {GamesPlayed} games played";
+
+
+            } 
+
 
         }
         class Player

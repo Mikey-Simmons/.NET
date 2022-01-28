14e5f89deb3f9c052d8055c89e98c53ac87770b0
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-01-27 23:07:02 -0800

    c#

---
+++ Animals/Animals/Program.cs
@@ -0,0 +1,117 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        // list of animals used to test
+        List<IAnimal> Animals = new List<IAnimal>();
+        var mydog = new Dog();
+        var mycat = new Cat();
+        var mycow = new Cow();
+        var mybact = new Bacterium();
+        Animals.Add(mydog);
+        Animals.Add(mycat);
+        Animals.Add(mycow);
+        Animals.Add(mybact);
+
+        //foreach loop to run through list of animals
+        foreach (IAnimal element in Animals)
+
+
+        {
+            //  check if animal is mammal or not and then run through all methods
+            var element1 = element as IMammal;
+            if (element1 is IMammal)
+            {
+                Console.WriteLine(element1.speak());
+                Console.WriteLine(element1.run());
+                Console.WriteLine(element1.eat());
+
+
+            }
+            else
+            {
+                Console.WriteLine(element.eat());
+
+
+            }
+
+
+
+        }
+
+    }
+    public interface IAnimal
+    {
+        public string eat();
+
+
+
+    }
+    public interface IMammal : IAnimal
+
+    {
+        string speak();
+        string run();
+    }
+
+    public class Dog : IMammal
+    {
+
+        public string speak()
+        {
+            return "Bark!";
+        }
+        public string run()
+        {
+            return "Dogs can run at a top speed of 45 mph!";
+        }
+        public string eat()
+        {
+            return "Dogs eat bones.";
+        }
+        public Dog()
+        {
+
+        }
+    }
+    public class Cat : IMammal
+    {
+        public string speak()
+        {
+            return "Meow!";
+        }
+        public string run()
+        {
+            return "Cats can run at a top speed of 30 mph!";
+        }
+        public string eat()
+        {
+            return "Cats eat Mice.";
+        }
+    }
+    public class Cow : IMammal
+    {
+        public string speak()
+        {
+            return "Moo!";
+        }
+        public string run()
+        {
+            return "Cows can run at a top speed of 25 mph!";
+        }
+        public string eat()
+        {
+            return "Cows eat grass.";
+        }
+    }
+    public class Bacterium : IAnimal
+    {
+
+        public string eat()
+        {
+            return "Bacteria eat viruses.";
+        }
+    }
+}
+
+
---
+++ ColorApp/ColorApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+
+    }
+}
+namespace Exercise
+{
+    class Color
+    {
+        public int Red;
+        public int Green;
+        public int Blue;
+        public int Opacity;
+        // default ctor
+        public Color()
+        {
+            Red = 0;
+            Green = 0;
+            Blue = 0;
+            Opacity = 255;
+        }
+        // ctor accepting 1 integer
+        public Color(int Red)
+        {
+            Red = Red;
+            Green = Red;
+            Blue = Red;
+            Opacity = 255;
+        }
+        // ctor accepting 3 integers
+        public Color(int Red, int Green, int Blue)
+        {
+            Red = Red;
+            Green = Green;
+            Blue = Blue;
+            Opacity = 255;
+        }
+        // ctor accepting 4 integers
+        public Color(int Red, int Green, int Blue, int Opacity)
+        {
+            Red = Red;
+            Green = Green;
+            Blue = Blue;
+            Opacity = Opacity;
+        }
+    }
+}

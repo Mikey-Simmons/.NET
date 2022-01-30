2d4487968712db5bb8675115a93aea5a1e22a8d9
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-01-29 20:46:17 -0800

    c#

--- AnotherProgram/AnotherProgram/Program.cs
+++ AnotherProgram/AnotherProgram/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using myNamespace;
+using otherNamespace;
+public class Program
+{
+   
+    public static void Main()
+    {
+        string s1 = printText();
+    }
+
+}
+namespace myNamespace
+{
+    public class AnotherProgram
+    {
+        public string printText() {
+            return "Hello Earth";
+        }
+
+    }
+}
+namespace otherNamespace
+{
+    public class AnotherProgramAgain
+    {
+        public string printSomeText()
+        {
+            return "Hello there!";
+        }
+    }
+}

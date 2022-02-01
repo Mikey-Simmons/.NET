eae6d4c836c5a140556043b3384b5ce66f46111e
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-02-01 06:22:53 -0800

    LambdaTesting

---
+++ LamdaTesting/LamdaTesting/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LambdaTesting
+{
+    class Program
+    {
+        static void Main(string[] args )
+        {
+           Action a;
+            a = test;
+            a();
+            
+            
+        }
+        static void test()
+        {
+            Console.WriteLine("This is  a test");
+        }
+        static Action Test2()
+        {
+            return test;
+        }
+    }
+    
+}
+
+// Lambda Expressions and predefined Delegates
+
+//Func= a variable that can be attached to a method that returns something
+//is computing something
+
+
+
+//Action = a variable that can be attached to a method that returns *void*
+//is doing something
+
+
+//Predicate= a variable that can be attached to a method returning a bool (True or False)
+//is answering a y/n or t/f question

031be8411c6f5b21a6d220182841f926a45c0168
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-02-07 18:44:44 -0800

    workingFinalProject

--- FinalProject/FinalProject/Program.cs
+++ FinalProject/FinalProject/Program.cs
@@ -9,75 +9,167 @@ namespace Part1
     static class TaxCalculator
     {
         // Create a static dictionary field that holds a List of TaxRecords and is keyed by a string
+        static Dictionary<string, List<TaxRecord>> states;
 
-        public static Dictionary<string, List<string>> TaxRecords = new Dictionary<string, List<string>>();
+        
 
 
         // create a static constructor that:
         static TaxCalculator()
         {
+            StreamReader reader = null;
+            try
+            {
+                states = new Dictionary<string, List<TaxRecord>>();
+                reader = File.OpenText("taxtable.csv");
+                string line;
+                do
+                {
+                    line = reader.ReadLine();
+                    try
+                    {
+                        TaxRecord tr = new TaxRecord(line);
+                        List<TaxRecord> records;
+                        bool found = states.TryGetValue(tr.StateCode, out records);
+                        if (found)
+                        {
+                            records.Add(tr);
+                        }
+                        else
+                        {
+                            records = new List<TaxRecord>();
+                            records.Add(tr);
+                            states.Add(tr.StateCode, records);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                        Console.WriteLine("Resuming...");
+                    }
+                    
+                }
+                while(!reader.EndOfStream);
+            }
+            catch (Exception ex)
+            { Console.WriteLine(ex); 
+              Console.WriteLine("Attempting to resume...");
+            }  
+
             
         }
-        // declare a streamreader to read a file
-        // enter a try/catch block for the entire static constructor to print out a message if an error occurs
-        // initialize the static dictionary to a newly create empty one
-        // open the taxtable.csv file into the streamreader
-        // loop over the lines from the streamreader
-        // read a line from the file
-        // constuct a taxrecord from the (csv) line in the file
-        // see if the state in the taxrecord is already in the dictionary
-        //     if it is:  add the new tax record to the list of records in that state
-        //     if it is not
-        //            create a new list of taxrecords
-        //            add the new taxrecord to the list
-        //            add the list to the dictionary under the state for the taxrecord
-        //provide a way to get out of the loop when you are done with the file....
-        // catch any exceptions while processing each line in another try/catch block located INSIDE the loop
-        //   this way if the line in the CSV file is incorrect, you will continue to process the next line
-        // make sure the streamreader is disposed no matter what happens.
+        public static decimal ComputeTaxFor(string state, decimal totalEarned)
+        {
+            List<TaxRecord> records;
+            bool found = states.TryGetValue(state, out records);
+            if(found)
+            {
+                Console.WriteLine($"Found {records.Count} records for state: {state}");
+                decimal totalTax = 0M;
+                foreach(TaxRecord tr in records)
+                {
+                    if(totalEarned >= tr.Floor && totalEarned <= tr.Ceiling)
+                    {
+                        decimal incomeForThisBracket = totalEarned - tr.Floor;
+                        decimal thisBracket = incomeForThisBracket * tr.Rate;
+                        Console.WriteLine($"Found Record {tr} [income: {incomeForThisBracket} Tax: {thisBracket}] Total Tax So Far: {totalTax +thisBracket}");
+                        return totalTax + (totalEarned - tr.Floor)* tr.Rate;
 
+                    }
+                    else
 
-        // create a static method (ComputeTaxFor)  to return the computed tax given a state and income
-        //  use the state as a key to find the list of taxrecords for that state
-        //   throw an exception if the state is not found.
-        //   otherwise use the list to compute the taxes
-
-        //  Create a variable to hold the final computed tax.  set it to 0
-        //  loop over the list of taxrecords for the state
-        //     check to see if the income is within the tax bracket using the floor and ceiling properties in the taxrecord
-        //     if NOT:  (the income is greater than the ceiling)
-        //        compute the total tax for the bracket and add it to the running total of accumulated final taxes
-        //        the total tax for the bracket is the ceiling minus the floor times the tax rate for that bracket.  
-        //        all this information is located in the taxrecord
-        //        after adding the total tax for this bracket, continue to the next iteration of the loop
-        //     IF The income is within the tax bracket (the income is higher than the floor and lower than the ceiling
-        //        compute the final tax by adding the tax for this bracket to the accumulated taxes
-        //        the tax for this bracket is the income minus the floor time the tax rate for this bracket
-        //        this number is the total final tax, and can be returned as the final answer
-
-
-        // try to figure out how to implement the Verbose option AFTER you have everything else done.
-
+                    {
+                        decimal incomeForThisBracket = tr.Ceiling-tr.Floor;
+                        decimal thisBracket = incomeForThisBracket * tr.Rate;
+                        Console.WriteLine($"Found record {tr} this Bracket: [income: {incomeForThisBracket} Tax: {thisBracket}] Total tax: {totalTax+thisBracket }");
+                        totalTax = +thisBracket;
+                    }
+                }
+                throw new Exception($"Income was higher than the tax ceiling: {totalEarned}");
+            }
+            else
+            {
+                throw new Exception("There was an error...");
+            }
+        }
+        
 
     }  // this is the end of the Tax Calculator
 
 
     class TaxRecord
     {
-        // create a TaxRecord class representing a line from the file.  It shoudl have public properties of the correct type
-        // for each of the columns in the file
-        //  StateCode   (used as the key to the dictionary)
-        //  State       (Full state name)
-        //  Floor       (lowest income for this tax bracket)
-        //  Ceiling     (highest income for this tax bracket )
-        //  Rate        (Rate at which income is taxed for this tax bracket)
-        //
-        //  Create a ctor taking a single string (a csv) and use it to load the record
-        //  Be sure to throw detailed exceptions when the data is invalid
-        //
-        //  Create an override of ToString to print out the tax record info nicely
+        public string StateCode
+        {
+            get;init;
+        }
 
+        public string State
+        {
+            get;init;
+        }
+        public decimal Floor
+        {
+            get; init;
+        }
+        public decimal Ceiling
+        {
+            get; init;
+        }
+        public decimal Rate
+        {
+            get; init;
+        }
+        public decimal YearlyPay
+        {
+            get; init;
+        }
 
+        public TaxRecord(string csv)
+        {
+            string[] items = csv.Split(',');
+            if(items.Length != 5)
+            {
+                throw new Exception($"csv doesn't have 5 fields seperated by commas... ");
+            }
+            // assigning state code variable
+            StateCode = items[0];
+            // asssigning state variable
+            State = items[1];
+            decimal a;
+            if(decimal.TryParse(items[2], out a))
+            {
+                Floor = a;
+            }
+            else
+            {
+                throw new Exception($"item Floor: 3rd is not a decimal [{items[2]} line=[{csv}]");
+            }
+            //declaring ceiling var
+            decimal b;
+            if(decimal.TryParse(items[3], out b))
+            {
+                Ceiling = b;
+            }
+            else
+            {
+                throw new Exception($"item ceiling: 4th is not a decimal [{items[3]} line=[{csv}]");
+            }
+            //rate var
+            decimal c;
+            if(decimal.TryParse(items[4],out c))
+            {
+                Rate = c;
+            }
+            else
+            {
+                throw new Exception($"item ceiling: 5th is not a decimal [{items[4]} line=[{csv}]");
+            }
+        }
+        public override string ToString()
+        {
+            return  $"Tax Record for {StateCode} {State} Floor: {Floor,15:000000000.000} Ceiling:{Ceiling,15:000000000.000} Rate:{Rate:00.000000}";
+        }
     }  // this is the end of the TaxRecord
 
     class Program
@@ -99,7 +191,19 @@ namespace Part1
             {
                 try
                 {
-                    // put your logic here
+                    Console.WriteLine("enter a state abbreviation");
+                    string state = Console.ReadLine().ToUpper();
+                    Console.WriteLine("Enter an income");
+                    string line;
+                    decimal income;
+                    // validating data
+                    while(!decimal.TryParse(line = Console.ReadLine(),out income))
+                    {
+                        Console.WriteLine("income was not a deceimal...");
+                        Console.WriteLine("please enter a decimal");
+                    }
+                    decimal tax = TaxCalculator.ComputeTaxFor(state, income);
+                    Console.WriteLine(tax);
                 }
                 catch (Exception ex)
                 {
